7ff433f791d0609dae68f4b701ac6edce26d78a7
Linden Bryon <linden@rushcliff.com>
2020-12-02 19:32:46 +0000

    converting a jpeg image into a bmp file

--- helpers/ImageHelper.js
+++ helpers/ImageHelper.js
@@ -1,6 +1,36 @@
+const fs = require('fs');
+var buffer = new Buffer.alloc(1024); 
+var Jimp = require('jimp');
 class ImageHelper {
-    constructor(file, filterType = "greyscale"){
-        this.file = file;
-        console.log("imagefile", file);
+    constructor(){
+        this.file = null;
+        this.bmpFile = null;
+        this.twodiarray = null;
+    }
+    convertImageToBMP(){
+        console.log("inside convert");
+        this.bmpFile = 'newfile-'+Date.now()+'.bmp';
+        return new Promise((resolve, reject) => {
+            Jimp.read('./public/uploads/' + this.file)
+            .then(lenna => {
+                lenna
+                .resize(256, 256) // resize
+                .quality(60) // set JPEG quality
+                .write('./public/uploads/' + this.bmpFile); // save
+                resolve();
+            })
+            .catch(err => {
+                reject(err);
+            });
+        });
+    }   
+    setImage(){
+        this.file = fileName;
+        this.convertImageToBMP(file).then(function(){
+            console.log("bmp done");
+        }).catch(function(err){
+            console.log("err", err);
+        })
     }
 }
+exports.ImageHelper = ImageHelper
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 const multer = require('multer');
 const path = require('path');
+var imageHelper = require('../helpers/ImageHelper');
+var imageHelperInstance = new imageHelper.ImageHelper();
+
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './public/uploads')
@@ -13,23 +16,35 @@ var storage = multer.diskStorage({
  
 var upload = multer({ storage: storage })
 
-const imageFilter = function(req, file, cb) {
-  // Accept images only
+const imageFilter = function(file) {
   if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-      req.fileValidationError = 'Only image files are allowed!';
-      return cb(new Error('Only image files are allowed!'), false);
+      return false;
   }
-  cb(null, true);
+  return true;
 };
-//var fs = require('fs'); 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.post('/upload', upload.single('profile_pic'), function (req, res, next) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
-  console.log(req.file);
+  imageHelperInstance.setImage(req.file.filename);
+  // if(!req.file){
+  //   var error = 'No File Uploaded'
+  // }
+  // //re-visit this later
+  // if(!imageFilter(req.file.filename)){
+  //   error = 'incorrect file format';
+  // };
+
+  
+
+
+  // res.render('index', 
+  //     { 
+  //       title: 'Express',
+  //       error: error
+  //     }
+  //   );
   
 })
 module.exports = router;
